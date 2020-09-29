fixed jacobian

examples/relations.py
@@ -50,8 +50,10 @@ def main():
             slam_data = np.fromstring(slam_data, dtype=np.float, sep=' ' )  
             timestamp = slam_data[0]
             if np.abs(timestamp - timestamp_init) < 1e-3:
+                timestamp_init_log = timestamp
                 pose_init = np.array([slam_data[1], slam_data[2], slam_data[3]])
             elif np.abs(timestamp - timestamp_final) < 1e-3:
+                timestamp_final_log = timestamp
                 pose_final = np.array([slam_data[1], slam_data[2], slam_data[3]])
             if pose_init is not None and pose_final is not None:
                 break
@@ -66,7 +68,7 @@ def main():
         translation_error = np.vstack([translation_error, relative_pose_error[0:2]])
         orientation_error = np.vstack([orientation_error, relative_pose_error[2]])
         counter = counter + 1
-        print(miss, counter, np.mean(np.linalg.norm(translation_error, axis=1)))            
+        print(miss, counter, timestamp_init_log - timestamp_init, timestamp_final_log - timestamp_final, np.mean(np.linalg.norm(translation_error, axis=1)))            
 
     print('----------------------------')
     print(np.mean(np.linalg.norm(translation_error, axis=1)), np.std(np.linalg.norm(translation_error, axis=1)))

examples/spline_slam_from_log.py
@@ -27,7 +27,7 @@ def main():
                         'surface_size': np.array([150.,150.]),
                         'min_angle': -90*np.pi/180., # -130*np.pi/180,
                         'max_angle': 90*np.pi/180., #129.75*np.pi/180,
-                        'angle_increment': 1.*np.pi/180., #.25*np.pi/180,
+                        'angle_increment': 1.*np.pi/361., #.25*np.pi/180,
                         'range_min': 0.1,
                         'range_max': 49.9, 
                         'logodd_occupied': 1., #logodd_occupied[res],#1 - .2*(nb_resolution - res - 1), #./(2**(nb_resolution - res - 1)),
@@ -77,7 +77,7 @@ def main():
     y, x = np.mgrid[-60:10+dy:dy, -40:40+dx:dx] # ACES
     #y, x = np.mgrid[-25:10+dy:dy, -15:25+dx:dx] # INTEL
     #y, x = np.mgrid[-15:15+dy:dy, -30:25+dx:dx] # FREIBURG
-    #y, x = np.mgrid[-25:50+dy:dy, -20:40+dx:dx] # MIT-CSAIL
+    y, x = np.mgrid[-25:50+dy:dy, -20:40+dx:dx] # MIT-CSAIL
     #y, x = np.mgrid[-100:150+dy:dy, -220:50+dx:dx] # MIT-KILLIAN
 
     #y, x = np.mgrid[-12.5:5+dy:dy, -5:5+dx:dx]
@@ -97,7 +97,7 @@ def main():
         pose = data[1:4]
         ranges = data[4:]
         for res in range(0, nb_resolution):
-            if num < 2:
+            if num < 5:
                 previous_pose = pose
                 multi_res_localization[res].pose = np.array([0,0,0])
                 path = np.array([0,0]).reshape(2,1)
@@ -146,16 +146,16 @@ def main():
         for res in range(0, nb_resolution):
             mapping_time += np.sum(multi_res_mapping[res].time)
             localization_time += np.sum(multi_res_localization[res].time)
-        #print(timestamp, pose[0], pose[1], pose[2])
-        print( num, 
-            1./(mapping_time/num), 
-            1./(localization_time/num), 
-            1./(mapping_time/num + localization_time/num) ) 
+        print(timestamp, pose[0], pose[1], pose[2])
+        # print( num, 
+        #     1./(mapping_time/num), 
+        #     1./(localization_time/num), 
+        #     1./(mapping_time/num + localization_time/num) ) 
 
         
 
         ########## Plotting #################
-        if (frame_counter > 0  and (num == 7373 or num == 1987 or num==13630 or num==4933 or num==7060)) or (frame_counter> 200):          
+        if (frame_counter > 0  and (num == 7373 or num == 1987 or num==13630 or num==4933 or num==7060)) or (frame_counter> 450):          
             offset = nb_resolution-nb_resolution_plot
             if frame_nb < -1:
                 frame_nb +=1
@@ -200,7 +200,7 @@ def main():
                     # Saving
                     axs[res-offset].axis('equal')
 
-                    plt.savefig('image/aces/' + input_param + '_' + str(frame_nb).zfill(4) + '.png')
+                    plt.savefig('image/intel/' + input_param + '_' + str(frame_nb).zfill(4) + '.png')
                     toc = time.time()
                 frame_counter = 0
                 frame_nb +=1

spline_slam/spline/spline_localization.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import time
+from scipy.optimize import least_squares
 
 class SplineLocalization:
     def __init__(self, spline_map, **kwargs): 
@@ -61,133 +62,52 @@ class SplineLocalization:
         return np.matmul(R, local) + pose[0:2].reshape(2,1)
     
     """ Estimate pose (core function) """
-    def compute_pose(self, map, pose_estimate, pts_occ_local, nb_iteration_max, gradient_step_size = 0.1):
-        # Initializing parameters
-        nb_iterations = 0.
-        residue = np.inf
- 
-        alpha = 2.
-        self.c = 1. #1 #2./3 #/3
+    def compute_pose(self, pose_estimate, pts_occ_local, ftol=1e-5):
+        res = least_squares(self.scipy_cost_function, 
+                            pose_estimate,
+                            jac = self.scipy_jacobian, 
+                            verbose = 0, 
+                            method='dogbox',
+                            loss='cauchy',
+                            ftol = ftol,
+                            f_scale = 3./2,
+                            args=pts_occ_local )
+        return res.x, res.cost
 
-        has_updated = True
-        nb_updates = 0
-        while (np.abs(residue) > .0001) and nb_iterations < nb_iteration_max:
-            if has_updated:
-                df, cost = self.compute_pose_increment(map, pts_occ_local, pose_estimate, alpha=alpha, c=self.c)
-                if np.linalg.norm(df[0:2]) > self.map.knot_space:
-                    df = self.map.knot_space*df/np.linalg.norm(df[0:2])
-
-            # Compute pose 
-            delta_pose = gradient_step_size*df
-            pose_estimate_candidate = pose_estimate - delta_pose
-            cost_candidate = self.cost_function(map, pts_occ_local, pose_estimate_candidate, alpha=alpha, c=self.c)
-            residue =  cost - cost_candidate
-
-            if residue > 0 :
-                gradient_step_size = 2.25*gradient_step_size
-                pose_estimate = pose_estimate_candidate
-                cost = cost_candidate
-                has_updated = True
-                nb_updates += 1
-                nb_iterations += 1
- 
-            else:
-                gradient_step_size /= 4
-                has_updated = False
-
-        return pose_estimate, cost, gradient_step_size
-
-    def cost_function(self, map, pts_occ_local, pose, alpha, c):
-        # computing alignment error
-        pts_occ = self.local_to_global_frame(pose, pts_occ_local)
-        c_index_occ = self.map.compute_sparse_tensor_index(pts_occ)
-        B_occ, _, _ = self.map.compute_tensor_spline(pts_occ, ORDER=0x01)        
-        s_occ = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)        
-        e_occ = (1 - s_occ/self.logodd_max_occupied)
-
-        if alpha == 2:   # squared error (L2) loss function
-            r = .5*(e_occ/c)**2
-        elif alpha == 0:   # Cauchy (aka  Lorentzian) loss 
-            r = np.log( .5*(e_occ/c)**2 +1  )
-        elif alpha == -np.inf:  # Welsch (aka Leclerc) loss function
-            r = 1 - np.exp(-.5 * (e_occ/c)**2)
-        else:
-            r =  (abs(alpha-2)/alpha)  * ( (((e_occ/c)**2)/abs(alpha-2) + 1)**(alpha/2.) - 1)
-
-        return np.sum(r)
-
-    def compute_pose_increment(self, map, pts_occ_local, pose, alpha, c):
+    def scipy_jacobian(self, pose, pts_occ_local_x, pts_occ_local_y):
+        pts_occ_local = np.vstack([pts_occ_local_x, pts_occ_local_y])
         # Transforming occupied points to global frame
-        pts_occ_global = self.local_to_global_frame(pose, pts_occ_local)
+        pts_occ = self.local_to_global_frame(pose, pts_occ_local)
         # Spline tensor
-        c_index_occ = self.map.compute_sparse_tensor_index(pts_occ_global)
-        B_occ, dBx_occ, dBy_occ = self.map.compute_tensor_spline(pts_occ_global, ORDER=0x01 | 0x02)                
-        #c_index_occ, B_occ, dBx_occ, dBy_occ, ddBx_occ, ddBy_occ, ddBxy_occ = self.compute_tensor_spline(map, pts_occ_global)
-        # Current value on the map
-        s_occ = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1) 
-        # Alginment error
-        e_occ = (1 - (s_occ/self.logodd_max_occupied))            
-        n_occ = len(e_occ)
+        c_index_occ = self.map.compute_sparse_tensor_index(pts_occ)
+        _, dBx_occ, dBy_occ = self.map.compute_tensor_spline(pts_occ, ORDER= 0x02)                
         # Rotation matrix
         cos, sin = np.cos(pose[2]), np.sin(pose[2])
         R = np.array([[-sin, -cos],[cos, -sin]])           
         # compute H and b  
-        ds_occ = np.zeros([2, n_occ])
-        ds_occ[0,:]=np.sum(self.map.ctrl_pts[c_index_occ]*dBx_occ, axis=1)
-        ds_occ[1,:]=np.sum(self.map.ctrl_pts[c_index_occ]*dBy_occ, axis=1)
+        ds_occ = np.zeros([2, len(pts_occ_local_x)])
+        ds_occ[0,:]=np.sum((self.map.ctrl_pts[c_index_occ]/ self.logodd_max_occupied) *dBx_occ, axis=1) 
+        ds_occ[1,:]=np.sum((self.map.ctrl_pts[c_index_occ]/ self.logodd_max_occupied) *dBy_occ, axis=1) 
         dpt_occ_local = R@pts_occ_local
     
-        # JAcobian
-        h_occ = np.zeros([3,n_occ])
-        h_occ[0,:] = -ds_occ[0,:]
-        h_occ[1,:] = -ds_occ[1,:]
-        h_occ[2,:] = -np.sum(dpt_occ_local*ds_occ,axis=0)
+        # Jacobian
+        h_occ = np.zeros([3, len(pts_occ_local_x)])
+        h_occ[0,:] = ds_occ[0,:]
+        h_occ[1,:] = ds_occ[1,:]
+        h_occ[2,:] = np.sum(dpt_occ_local*ds_occ,axis=0)
 
-        if alpha == 2:   # Welsch/Leclerc loss function
-            r = .5*(e_occ/c)**2
-            df = (1./c**2)*np.sum( e_occ * h_occ, axis = 1)
-            # dfx = (1./c**2)*( e_occ )
-            # ddfx = (1./c**2)
+        return h_occ.T
 
-        elif alpha == 0:
-            r = np.log( .5*(e_occ/c)**2 +1  )
-            df = np.sum( 2* e_occ /(e_occ**2 + 2*c**2)* h_occ, axis = 1)
-            # dfx =  2* e_occ /(e_occ**2 + 2*c**2)
-            # ddfx =  2* (-e_occ**2 + 2*c**2) /((e_occ**2 + 2*c**2)**2)
+    def scipy_cost_function(self, pose, pts_occ_local_x, pts_occ_local_y):
+        # computing alignment error
+        pts_occ_local = np.vstack([pts_occ_local_x, pts_occ_local_y])
+        pts_occ = self.local_to_global_frame(pose, pts_occ_local)
+        c_index_occ = self.map.compute_sparse_tensor_index(pts_occ)
+        B_occ, _, _ = self.map.compute_tensor_spline(pts_occ, ORDER=0x01)        
+        s_occ = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)        
+        r = (1 - s_occ/self.logodd_max_occupied)
+        return r
 
-        elif alpha == -np.inf:
-            z = np.exp(-.5*(e_occ/c)**2)
-            r = 1 - z
-            df = (1./c**2)*np.sum( e_occ * z * h_occ, axis = 1)  
-            # dfx = (1./c**2)*( e_occ * z )
-            # ddfx = (z/c**2)*(1 - (e_occ/c)**2 )
-        else:
-            z = ((e_occ/c)**2 / abs(alpha-2)) + 1
-            r =  abs(alpha-2)/alpha  * (z**(alpha/2.) - 1)
-            df = (1./c**2)*np.sum( e_occ * z**(alpha/2.-1) * h_occ, axis = 1)
-            # dfx = (1./c**2)*( e_occ * (z**(alpha/2.-1)) )            
-            # ddfx = (1./c**2)* ( (z**(alpha/2.-1)) + np.sign(alpha-2)* (z**(alpha/2.-2)) *(e_occ/c)**2  ) 
-
-        # H = np.zeros([3,3])
-        # sxx = dfx*np.sum(map.ctrl_pts[c_index_occ]*ddBx_occ, axis=1)
-        # syy = dfx*np.sum(map.ctrl_pts[c_index_occ]*ddBy_occ, axis=1)
-        # sxy = dfx*np.sum(map.ctrl_pts[c_index_occ]*ddBxy_occ, axis=1)                
-        # sxtheta =  dpt_occ_local[0,:]*sxx + dpt_occ_local[1,:]*sxy
-        # sytheta =  dpt_occ_local[0,:]*sxy + dpt_occ_local[1,:]*syy
-        # sthetatheta = dpt_occ_local[0,:]*sxtheta + dpt_occ_local[1,:]*sytheta
-
-        # H[0,0] = np.sum(sxx)
-        # H[1,1] = np.sum(syy)
-        # H[2,2] = np.sum(sthetatheta)
-        # H[0,1] = H[1,0] = np.sum(sxy)
-        # H[0,2] = H[2,0] = np.sum(sxtheta)
-        # H[1,2] = H[2,1] = np.sum(sytheta)
-
-        # ddf = ( (ddfx*h_occ) @ h_occ.T + H)         
-        #if np.linalg.det(ddf) > self.det_Hinv_threshold:
-        #    df = np.linalg.inv(ddf)@df   
-        cost = np.sum(r)
-        return df, cost 
 
 
     """"Occupancy grid mapping routine to update map using range measurements"""
@@ -206,23 +126,23 @@ class SplineLocalization:
         #pts_free_local = self.detect_free_space(ranges)
         self.time[1] += time.time() - tic
         # Localization
-        tic = time.time()
-        
+        tic = time.time()      
         best_cost_estimate = np.inf
         if unreliable_odometry:
-            candidate = [0, np.pi/4., -np.pi/4., np.pi/2., -np.pi/2]
+            candidate = [0, np.pi/4., -np.pi/4., np.pi/2., -np.pi/2, -1.5*np.pi, -1.5*np.pi]
         else:
             candidate = [0]
         for theta in candidate:
-            pose_estimate_candidate, cost_estimate, step_self = self.compute_pose(map, np.array(pose_estimative) + np.array([0,0,theta]), pts_occ_local, nb_iteration_max=5)
+            pose_estimate_candidate, cost_estimate = self.compute_pose(np.array(pose_estimative) + np.array([0,0,theta]), pts_occ_local, ftol=1e-2)
             if cost_estimate < best_cost_estimate:
                 best_cost_estimate = cost_estimate
                 best_pose_estimate = pose_estimate_candidate
-                best_step_estimate = step_self
-        pose_self, cost_self, step_self = self.compute_pose(map,  self.pose, pts_occ_local, nb_iteration_max=5)        
+        pose_self, cost_self= self.compute_pose(self.pose, pts_occ_local, ftol = 1e-2)        
 
         if best_cost_estimate < cost_self:
-            self.pose, _, _ = self.compute_pose(map, best_pose_estimate, pts_occ_local, nb_iteration_max=self.nb_iteration_max-10, gradient_step_size=best_step_estimate)
+            self.pose, _ = self.compute_pose(best_pose_estimate, pts_occ_local)
         else:
-            self.pose, _, _ = self.compute_pose(map, pose_self, pts_occ_local, nb_iteration_max=self.nb_iteration_max-10, gradient_step_size=step_self)
+            self.pose, _ = self.compute_pose(pose_self, pts_occ_local)
+
+        #self.pose, _ = self.compute_pose(pose_estimative, pts_occ_local)
         self.time[2] += time.time() - tic

spline_slam/spline/spline_map_recursive.py
@@ -89,31 +89,32 @@ class SplineMap:
         c_index_occ = self.map.compute_sparse_tensor_index(pts_occ)
 
         B_occ, _, _ = self.map.compute_tensor_spline(pts_occ, ORDER=0x01)
-        #s_est_occ_ant = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)
+        s_est_occ_ant = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)
 
         c_index_occ_free = np.intersect1d(c_index_free, c_index_occ)
         self.map.ctrl_pts[c_index_free] -= self.logodd_free
         self.map.ctrl_pts[c_index_occ_free] += .5*self.logodd_free
 
         #Occupied space 
-        # for i in range(0, pts_occ.shape[1]):
-        #     if i < pts_occ.shape[1]-1:
-        #         d = np.linalg.norm(pts_occ[:,i+1] - pts_occ[:,i])    
-        #     else:
-        #         d = np.linalg.norm(pts_occ[:, i] - pts_occ[:, i-1])
-        #     d = (min(d/(4*self.knot_space),1))
-        #     s_est_occ = np.sum(self.ctrl_pts[c_index_occ[i,:]]*B_occ[i,:])   
-        #     e_occ = min(self.logodd_max_occupied, (s_est_occ_ant[i] + self.logodd_occupied))-s_est_occ 
-        #     B_occ_norm = np.linalg.norm(B_occ[i,:])
-        #     B_occ_norm_squared = B_occ_norm**2
-        #     mag_occ =  e_occ /B_occ_norm_squared
-        #     np.add.at(self.ctrl_pts, c_index_occ[i,:], d*(B_occ[i,:]*mag_occ))
-        s_est_occ = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)   
-        e_occ = (self.logodd_max_occupied - s_est_occ) 
-        B_occ_norm = np.linalg.norm(B_occ, axis=1)
-        B_occ_norm_squared = B_occ_norm**2
-        mag_occ =  np.minimum(self.logodd_occupied/B_occ_norm_squared, np.abs(e_occ)) * np.sign(e_occ)
-        np.add.at(self.map.ctrl_pts, c_index_occ, (B_occ.T*mag_occ).T)        
+        for i in range(0, pts_occ.shape[1]):
+            if i < pts_occ.shape[1]-1:
+                d = np.linalg.norm(pts_occ[:,i+1] - pts_occ[:,i])    
+            else:
+                d = np.linalg.norm(pts_occ[:, i] - pts_occ[:, i-1])
+            d = (min(d/(4*self.map.knot_space),1))
+            s_est_occ = np.sum(self.map.ctrl_pts[c_index_occ[i,:]]*B_occ[i,:])   
+            e_occ = min(self.logodd_max_occupied, (s_est_occ_ant[i] + self.logodd_occupied))-s_est_occ 
+            B_occ_norm = np.linalg.norm(B_occ[i,:])
+            B_occ_norm_squared = B_occ_norm**2
+            mag_occ =  e_occ /B_occ_norm_squared
+            np.add.at(self.map.ctrl_pts, c_index_occ[i,:], d*(B_occ[i,:]*mag_occ))
+
+        # s_est_occ = np.sum(self.map.ctrl_pts[c_index_occ]*B_occ, axis=1)   
+        # e_occ = (self.logodd_max_occupied - s_est_occ) 
+        # B_occ_norm = np.linalg.norm(B_occ, axis=1)
+        # B_occ_norm_squared = B_occ_norm**2
+        # mag_occ =  np.minimum(self.logodd_occupied/B_occ_norm_squared, np.abs(e_occ)) * np.sign(e_occ)
+        # np.add.at(self.map.ctrl_pts, c_index_occ, (B_occ.T*mag_occ).T)        
 
         # Control points index 
         c_index_min = min(np.min(c_index_occ[:,0]), np.min(c_index_free[:,0]))